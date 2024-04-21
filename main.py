22f36f5046f958dc0c30b8288b02ac7159f188bc
AmatorAVG <agushcha@team.vtbconnect.ru>
2024-04-21 12:34:32 +0700

    Changed stars animation

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import curses
 
 async def blink(canvas, row, column, symbol='*'):
     while True:
+        for i in range(random.randint(1, 5)):
+            await asyncio.sleep(0)
+
         canvas.addstr(row, column, symbol, curses.A_DIM)
         for i in range(15):
             await asyncio.sleep(0)
